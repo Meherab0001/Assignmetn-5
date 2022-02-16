0dfb1eca94e88c448061cf6548bce7d99e8e858a
Meherab211 <meherab395@gmail.com>
2022-02-17 00:57:01 +0600

    basic calc

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
-console.log('this app');
+const totalCost=document.getElementById('calculate');
+
+
+
+
+
+totalCost.addEventListener('click',function(){
+    const foodCost=document.getElementById('food').value;
+const rentCost=document.getElementById('rent').value;
+const colthCost=document.getElementById('colth').value;
+
+const income=document.getElementById('income').value;
+    const totalExpenes=parseFloat(foodCost) +parseFloat(rentCost)+parseFloat(colthCost);
+    const expenes=document.getElementById('total-expenes');
+  expenes.innerHTML=totalExpenes;
+    const blance=document.getElementById('blance');
+     
+   const blanceTotal=parseFloat(income)-totalExpenes;
+   blance.innerHTML=blanceTotal;
+});
+
+
+
