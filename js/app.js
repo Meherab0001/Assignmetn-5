7d23f548e6a45849047ae87b48126bc5e0a44e1c
Meherab211 <meherab395@gmail.com>
2022-02-17 18:03:54 +0600

    compelete calc

--- js/app.js
+++ js/app.js
@@ -1,23 +1,87 @@
+
+ 
+
 const totalCost=document.getElementById('calculate');
 
-
-
-
-
 totalCost.addEventListener('click',function(){
-    const foodCost=document.getElementById('food').value;
-const rentCost=document.getElementById('rent').value;
-const colthCost=document.getElementById('colth').value;
+  /*   const foodCost=document.getElementById('food-cost').value;
+const rentCost=document.getElementById('rent-cost').value;
+const colthCost=document.getElementById('colth-cost').value;
 
 const income=document.getElementById('income').value;
-    const totalExpenes=parseFloat(foodCost) +parseFloat(rentCost)+parseFloat(colthCost);
+    const totalExpenes=parseInt(foodCost) +parseInt(rentCost)+parseInt(colthCost);
     const expenes=document.getElementById('total-expenes');
-  expenes.innerHTML=totalExpenes;
+  expenes.innerText=totalExpenes;
     const blance=document.getElementById('blance');
      
-   const blanceTotal=parseFloat(income)-totalExpenes;
-   blance.innerHTML=blanceTotal;
+   const blanceTotal=parseInt(income)-totalExpenes;
+   blance.innerText=blanceTotal; */
+   calculateTotal();
+   errorMessage()
 });
+// total calculation
+function calculateTotal(){
+   const foodCost=document.getElementById('food-cost').value;
+    const rentCost=document.getElementById('rent-cost').value;
+    const colthCost=document.getElementById('colth-cost').value;
+    
+   
+    const income=document.getElementById('income').value;
 
+  
+       
+            const totalExpenes=parseInt(foodCost) +parseInt(rentCost)+parseInt(colthCost);
+        const expenes=document.getElementById('total-expenes');
+        const expenesTotal=parseInt(totalExpenes);
+      expenes.innerText=expenesTotal;
+        const blance=document.getElementById('blance');
+         
+       const blanceTotal=parseInt(income)-totalExpenes;
+       blance.innerText=blanceTotal;
+    
+}
+// error messeage handle
+
+
+function errorMessage(){
+    
+    const income=document.getElementById('income').value;
+    const foodCost=document.getElementById('food-cost').value;
+    const rentCost=document.getElementById('rent-cost').value;
+    const colthCost=document.getElementById('colth-cost').value;
+    const blance=document.getElementById('blance');
+    const expenes=document.getElementById('total-expenes');
+    const calculateError=document.getElementById('calc');
+
+    if (income<expenes) {
+      return alert('Please enter the correct amount'); 
+    }
+   
+   
+}
+
+const savaingBtn=document.getElementById ('saving-btn').addEventListener('click',function(){
+
+   
+    const income=document.getElementById('income').value;
+
+    const savingPercent=document.getElementById('saving').value;
+   
+
+    const savingTotal=document.getElementById('saving-amount');
+    const savingAmount=parseInt(income) * parseInt(savingPercent) /100;
+    
+    savingTotal.innerText=savingAmount;
+    const blance=document.getElementById('blance');
+    
+    const remaingBlance=document.getElementById('remaing');
+  
+    const totalRemaingBlance=parseInt(blance.innerText) -savingAmount;
+    remaingBlance.innerText=totalRemaingBlance;
+
+ 
+   
+    
+})
 
 
