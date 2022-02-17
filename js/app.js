6d8465a3bb9bb835e12f1e6958f3d41c944d4ad6
Meherab211 <meherab395@gmail.com>
2022-02-17 20:52:41 +0600

    last calculation

--- js/app.js
+++ js/app.js
@@ -17,33 +17,45 @@ const income=document.getElementById('income').value;
    const blanceTotal=parseInt(income)-totalExpenes;
    blance.innerText=blanceTotal; */
    calculateTotal();
-   errorMessage()
+   errorMessage('income-error')
+   errorMessage('food-error')
+   errorMessage('rent-error')
+   errorMessage('colth-error')
+   
 });
+
+
 // total calculation
 function calculateTotal(){
    const foodCost=document.getElementById('food-cost').value;
     const rentCost=document.getElementById('rent-cost').value;
     const colthCost=document.getElementById('colth-cost').value;
-    
-   
+ 
     const income=document.getElementById('income').value;
 
-  
-       
-            const totalExpenes=parseInt(foodCost) +parseInt(rentCost)+parseInt(colthCost);
+    const totalExpenes=parseInt(foodCost) +parseInt(rentCost)+parseInt(colthCost);
         const expenes=document.getElementById('total-expenes');
         const expenesTotal=parseInt(totalExpenes);
       expenes.innerText=expenesTotal;
-        const blance=document.getElementById('blance');
+      const blance=document.getElementById('blance');
+      if (income<expenesTotal) {
+          return alert('please input the valid number');
          
-       const blanceTotal=parseInt(income)-totalExpenes;
-       blance.innerText=blanceTotal;
+      }
+     
+      else{
+        
+         
+        const blanceTotal=parseInt(income)-totalExpenes;
+        blance.innerText=blanceTotal;
+      }
+      
     
 }
 // error messeage handle
 
 
-function errorMessage(){
+function errorMessage(messeageError){
     
     const income=document.getElementById('income').value;
     const foodCost=document.getElementById('food-cost').value;
@@ -51,13 +63,22 @@ function errorMessage(){
     const colthCost=document.getElementById('colth-cost').value;
     const blance=document.getElementById('blance');
     const expenes=document.getElementById('total-expenes');
-    const calculateError=document.getElementById('calc');
-
-    if (income<expenes) {
-      return alert('Please enter the correct amount'); 
+    const incomeError=document.getElementById(messeageError);
+    const savingTotal=document.getElementById('saving-amount');
+    if (isNaN(income) || income<0 ||isNaN(foodCost) ||foodCost<0 || isNaN(colthCost) || colthCost<0 || isNaN(rentCost) || rentCost<0)  {
+        incomeError.style.display="block";
+        blance.innerText='';
+        expenes.innerText='';
+        
     }
-   
-   
+    if(blance.innerText<savingTotal.innerText){
+        return alert('you have not enough money for saving');
+        savingTotal.innerText="";
+    }
+
+
+  
+
 }
 
 const savaingBtn=document.getElementById ('saving-btn').addEventListener('click',function(){
@@ -72,13 +93,22 @@ const savaingBtn=document.getElementById ('saving-btn').addEventListener('click'
     const savingAmount=parseInt(income) * parseInt(savingPercent) /100;
     
     savingTotal.innerText=savingAmount;
+   
     const blance=document.getElementById('blance');
-    
-    const remaingBlance=document.getElementById('remaing');
+   
   
-    const totalRemaingBlance=parseInt(blance.innerText) -savingAmount;
-    remaingBlance.innerText=totalRemaingBlance;
-
+        
+    const remaingBlance=document.getElementById('remaing');
+    
+        
+  
+   
+        const totalRemaingBlance=parseInt(blance.innerText) -savingAmount;
+        remaingBlance.innerText=totalRemaingBlance;
+  
+   
+   
+        errorMessage()
  
    
     
